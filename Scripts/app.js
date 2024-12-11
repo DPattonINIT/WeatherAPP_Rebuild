2216248dc65843cebe4e4cd01f48f3be990a401e
DPattonINIT <DlamarPatton@gmail.com>
2024-12-10 21:41:40 -0800

    added "current Stockton data" + btn

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,9 +1,33 @@
+const DarrylKey = '2a81aae1fd6131a7dd0e3509c4a72374';
+
+let userInput = document.getElementById('userInput');
+let currentCity = document.getElementById('currentCity');
+let currentTemperature = document.getElementById('currentTemperature');
+let currentData = "";
+let weekData = "";
+
+
+
+searchBtn.addEventListener('click', function(){
+    console.log("working")
+let userInput = document.getElementById('userInput').value
+currentCity.innerText = currentData.name
+currentTemperature.innerText = currentData.main.temp + "°F"
+});
+
+
+
+
+
+
+
+
 
 async function dataCall() {
-    const response = await fetch('./data/data.json');
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=stockton&appid=${DarrylKey}&units=imperial`);
     const data = await response.json();
-    console.log(data.weatherReport[0])
-    // localData = data;
+    console.log(data)
+    currentData = data;
     // console.log(localData);   
 }
 dataCall();
