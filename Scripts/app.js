9be4b04ae90ff25b0ce448ddf35b08bb57199cb7
DPattonINIT <DlamarPatton@gmail.com>
2024-12-11 12:27:09 -0800

    update. Moved APIkey into gitingore file.

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,33 +1,62 @@
-const DarrylKey = '2a81aae1fd6131a7dd0e3509c4a72374';
+import { DarrylKey } from "../APIKey";
+
 
 let userInput = document.getElementById('userInput');
+let searchBtn = document.getElementById('searchBtn');
+
 let currentCity = document.getElementById('currentCity');
 let currentTemperature = document.getElementById('currentTemperature');
 let currentData = "";
 let weekData = "";
+let userLocation = "";
 
 
 
-searchBtn.addEventListener('click', function(){
-    console.log("working")
-let userInput = document.getElementById('userInput').value
-currentCity.innerText = currentData.name
-currentTemperature.innerText = currentData.main.temp + "°F"
-});
+async function retrieveData() {
+    userLocation = userInput.value.trim();
 
-
-
-
-
-
-
-
-
-async function dataCall() {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=stockton&appid=${DarrylKey}&units=imperial`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${DarrylKey}&units=imperial`);
     const data = await response.json();
-    console.log(data)
-    currentData = data;
-    // console.log(localData);   
+    console.log(userLocation);
 }
-dataCall();
+retrieveData();
+
+
+
+
+
+
+
+
+
+// searchBtn.addEventListener('click', function(){
+//     console.log("working")
+// let userInput = document.getElementById('userInput').value
+// currentCity.innerText = currentData.name
+// currentTemperature.innerText = currentData.main.temp + "°F"
+// });
+
+
+
+
+
+
+
+
+
+// async function dataCall() {
+//     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=stockton&appid=${DarrylKey}&units=imperial`);
+//     const data = await response.json();
+//     console.log(data)
+//     currentData = data;
+//     // console.log(localData);   
+// }
+// dataCall();
+
+// async function weatherCall(){
+// const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=Paris&appid=${DarrylKey}&units=imperial`);
+// const worlddata = await response.json();
+// console.log(worlddata)
+
+// }
+// weatherCall();
