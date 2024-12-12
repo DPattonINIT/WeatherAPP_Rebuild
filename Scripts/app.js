436b003e9213f5ab39da319495ad676a974f9434
DPattonINIT <DlamarPatton@gmail.com>
2024-12-12 06:39:09 -0800

    added wind, low temp, high temp for current weather

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,22 +9,40 @@ let searchBtn = document.getElementById('searchBtn');
 let currentCity = document.getElementById('currentCity');
 let currentTemperature = document.getElementById('currentTemperature');
 let userLocation = "";
+let todayTemp = document.getElementById('todayTemp');
+let todayLow = document.getElementById('todayLow');
+let todayWind = document.getElementById('todayWind');
+let todayPrec = document.getElementById('todayPrec');
 
 
 
 
+// Current weather Search-------------------------------------------------
 async function currentData(){
-    userLocation = userInput.value.trim();
+    userLocation = userInput.value.trim().toLowerCase();
 
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=${DarrylKey}&units=imperial`);
     const data = await response.json();
     console.log(data);
     currentCity.innerText = `${data.name}, ${data.sys.country}`;
     currentTemperature.innerText = `${data.main.temp}°F`;
-   }
+    todayTemp.innerText = `${data.main.temp_max}°F - H`;
+    todayLow.innerText = `${data.main.temp_min}°F -Low`;
+    todayWind.innerText = `${data.wind.speed} MPH - Wind`;
+}
 currentData();
 
+
+
+
+
+
+
+
 searchBtn.addEventListener('click', currentData);
+
+
+
 
 
 
