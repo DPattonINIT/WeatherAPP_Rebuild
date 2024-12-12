96764a4552aadf79f6ffcb46e30bd15ea2650856
DPattonINIT <DlamarPatton@gmail.com>
2024-12-12 06:16:17 -0800

    rewrote code. search working for current weather

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,26 +1,37 @@
 // import { DarrylKey } from "./APIKey";
 
 const DarrylKey = '2a81aae1fd6131a7dd0e3509c4a72374';
+
+
+
 let userInput = document.getElementById('userInput');
 let searchBtn = document.getElementById('searchBtn');
 let currentCity = document.getElementById('currentCity');
 let currentTemperature = document.getElementById('currentTemperature');
-
-
-let currentData = "";
-let weekData = "";
 let userLocation = "";
 
-async  function retrieveData(){
+
+
+
+async function currentData(){
     userLocation = userInput.value.trim();
 
-}
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${userLocation}&appid=${DarrylKey}&units=imperial`);
+    const data = await response.json();
+    console.log(data);
+    currentCity.innerText = `${data.name}, ${data.sys.country}`;
+    currentTemperature.innerText = `${data.main.temp}°F`;
+   }
+currentData();
+
+searchBtn.addEventListener('click', currentData);
 
 
 
 
 
 
+ 
 
 
 
